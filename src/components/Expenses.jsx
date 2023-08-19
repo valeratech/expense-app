ea50fec82d87513d3e394ed943093e5706bc6586
Ryan Valera <valeraryan@gmail.com>
2023-08-19 10:12:05 -0700

    Added the expenses class to the Expenses div component container to inherit styling from ExpenseItem.css

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -2,11 +2,11 @@ import ExpenseItem from "./ExpenseItem";
 
 function Expenses({expenses}) {
     return (
-        <>
+        <div className="expenses">
         {expenses.map(item => (
                 <ExpenseItem date={item.date} title={item.title} amount={item.amount} />
             ))}
-            </>
+            </div>
     )
 }
 
