416d0c69ae6e38f4964204776a30337230130a22
Ryan Valera <valeraryan@gmail.com>
2023-08-19 02:26:56 -0700

    Added ExpenseItem component which holds the expense-item information inside a div container. App.js contains an array of placeholder objects to populate the App and used for testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'Toilet Paper',
+            amount: 94.12,
+            date: new Date(2020, 7, 14).toString(),
+        },
+        {
+            id: 'e2', title: 'New TV',
+            amount: 799.49,
+            date: new Date(2021, 2, 12).toString(),
+        },
+        {
+            id: 'e3',
+            title: 'Car Insurance',
+            amount: 294.67,
+            date: new Date(2021, 2, 28).toString(),
+        },
+        {
+            id: 'e4',
+            title: 'New Desk (Wooden)',
+            amount: 450,
+            date: new Date(2021, 5, 12).toString()
+        },
+    ];
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            {expenses.map(item => (
+                <ExpenseItem date={item.date} title={item.title} amount={item.amount} />
+            ))}
+        </div>
+      );
 }
 
 export default App;
