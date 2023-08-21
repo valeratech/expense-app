07c9a5a6bca42371f0e96c4e10d5f2cba29288a8
Ryan Valera <valeraryan@gmail.com>
2023-08-21 08:38:43 -0700

    Added useState for the ExpenseForm values and an onChange event-eventlistener on inputs to check for input

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,35 @@
 import './ExpenseForm.css'
+import {useState} from "react";
 
 function ExpenseForm() {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    function inputChangeHandler(identifier, value) {
+        if (identifier === 'title') {
+            console.log(value);
+        } else if (identifier === 'amount') {
+            console.log(value);
+        } else if (identifier === 'date') {
+            console.log(value);
+        }
+    }
+
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' />
+                    <input type='text' onChange={(e) => inputChangeHandler('title', e.target.value)}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' />
+                    <input type='number' min='0.01' step='0.01' onChange={(e) => inputChangeHandler('amount', e.target.value)}/>
                 </div>
                 <div className='new-expense__control'>
-                    <label>Data</label>
-                    <input type='date' />
+                    <label>Date</label>
+                    <input type='date' onChange={(e) => inputChangeHandler('date', e.target.value)}/>
                 </div>
             </div>
             <div clasName='new-expense__actions'>
