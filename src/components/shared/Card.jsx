2f01e1956fcc4afc5c58aadc59fcdd1a2c0ac41b
Ryan Valera <valeraryan@gmail.com>
2023-09-09 20:55:01 -0700

    Removed the classes variable in Card.jsx and passed in the value directly to the className prop in the div element inside of the Card function.

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,9 +1,8 @@
 import './Card.css';
 
 function Card(props) {
-    const classes= 'card ' + props.className;
     return (
-        <div className={classes}>
+        <div className={'card ' + props.className}>
             {props.children}
         </div>
     )
