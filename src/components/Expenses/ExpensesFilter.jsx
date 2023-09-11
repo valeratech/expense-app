8128a452ae452c1ca935aaae33bee057e25300f6
Ryan Valera <valeraryan@gmail.com>
2023-09-11 13:44:04 -0700

    Added ExpensesFilter.jsx with starter html inputs which will be used to obtain the selected year.

--- src/components/Expenses/ExpensesFilter.jsx
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -7,7 +7,8 @@ const ExpensesFilter = () => {
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select>
+                <select onChange={()=> console.log('Change')}>
+                    <option value='2023'>2023</option>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
