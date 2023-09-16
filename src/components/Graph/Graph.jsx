f55c65bff42a6b31cf344b6456b6cd8f59cd358e
Ryan Valera <valeraryan@gmail.com>
2023-09-16 16:23:59 -0700

    Added code to the GraphBar.jsx component which will make up the html and styling for the bar. The 'style=height: percentage' acts as the mechanism to fill the bar. The props.label will be the caption or month for each bar.

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import GraphBar from './GraphBar';
 import './Graph.css';
 
@@ -6,8 +5,7 @@ function Graph (props) {
 
     return (
         <div className='graph'>
-            {/*The graphData will be an array of objects that contain "label" and "value" keys. */}
-            {/*The values will be a month and the total amount for each month respectively*/}
+
             {props.graphData.map(item => (
                 // The maxValue will represent the total (max value) sum of ALL values filtered for the given Month
                 <GraphBar key={label} value={value} maxValue={null} label={label}/>
--- src/components/Graph/GraphBar.jsx
+++ src/components/Graph/GraphBar.jsx
@@ -1,5 +1,21 @@
-function GraphBar() {
+import './GraphBar.css'
 
-}
+function GraphBar(props) {
+
+    return (
+        <div className='graph-bar'>
+            {/*The graph-bar__inner div acts as the container for the actual bar for each month */}
+            <div className='graph-bar__inner'>
+                <div
+                    // The graph-bar__fill div displays and visibly fills the graph-bar__inner container by percentage
+                    className='graph-bar__fill'
+                    style={{ height: 100% }}
+                ></div>
+            </div>
+            {/*The label represents the caption for each month that displays under each bar*/}
+            <div className='graph-bar__label'>{props.label}</div>
+        </div>
+    );
+};
 
 export default GraphBar;
