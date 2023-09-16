727d79348100552c3a26c82c0c0a1c24d7c74735
Ryan Valera <valeraryan@gmail.com>
2023-09-16 15:48:00 -0700

    Created parameters/props for the Graph.jsx component. An array of objects that contain 'label' and 'value' keys will be created in another component to be iterated through as props. These props will be passed to the GraphBar.jsx component to create and fill a percentage of each 'bar'. The bar will be made up of nested div containers.

--- src/components/Graph/Graph.jsx
+++ src/components/Graph/Graph.jsx
@@ -1,5 +1,19 @@
-function Graph() {
+import React from 'react';
+import GraphBar from './GraphBar';
+import './Graph.css';
 
+function Graph (props) {
+
+    return (
+        <div className='graph'>
+            {/*The graphData will be an array of objects that contain "label" and "value" keys. */}
+            {/*The values will be a month and the total amount for each month respectively*/}
+            {props.graphData.map(item => (
+                // The maxValue will represent the total (max value) sum of ALL values filtered for the given Month
+                <GraphBar key={label} value={value} maxValue={null} label={label}/>
+            ))}
+        </div>
+    )
 }
 
 export default Graph;
