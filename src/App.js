8c057ab66ff79c1a0238666aebb509c2bece72b3
Ryan Valera <valeraryan@gmail.com>
2023-09-17 11:06:24 -0700

    Changed the order of components in App.js. NewExpense.jsx component was moved to the bottom. Expenses.jsx moved to the top.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
 
     return (
         <div>
-            <NewExpense onUpdateExpenseItems={updateExpenseItems} />
             <Expenses expenseData={expenseItems} />
+            <NewExpense onUpdateExpenseItems={updateExpenseItems} />
         </div>
       );
 }
