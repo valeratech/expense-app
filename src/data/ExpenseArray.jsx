ae33a5cecffcdb674f0d0838b7ed615d0c12bf70
Ryan Valera <valeraryan@gmail.com>
2023-09-17 11:28:03 -0700

    Added more expense-object to the ExpenseArray.jsx data component to statically create more examples.

--- src/data/ExpenseArray.jsx
+++ src/data/ExpenseArray.jsx
@@ -22,6 +22,121 @@ const expenses = [
         amount: 450,
         date: new Date(2021, 5, 12)
     },
+    {
+        id: 'e5',
+        title: 'Paper Towels',
+        amount: 9.12,
+        date: new Date(2022, 5, 4)
+    },
+    {
+        id: 'e6', title: 'PC',
+        amount: 3799.49,
+        date: new Date(2023, 9, 16)
+    },
+    {
+        id: 'e7',
+        title: 'Car',
+        amount: 34807.55,
+        date: new Date(2023, 1, 5)
+    },
+    {
+        id: 'e8',
+        title: 'Trucker Hat)',
+        amount: 14.99,
+        date: new Date(2021, 12, 24)
+    },
+    {
+        id: 'e9',
+        title: 'Conditioner',
+        amount: 35.77,
+        date: new Date(2023, 8, 14)
+    },
+    {
+        id: 'e10', title: 'Teething Toy',
+        amount: 18.42,
+        date: new Date(2019, 7, 11)
+    },
+    {
+        id: 'e11',
+        title: 'Keyboard',
+        amount: 149.23,
+        date: new Date(2021, 4, 12)
+    },
+    {
+        id: 'e12',
+        title: 'Foam roller',
+        amount: 14.99,
+        date: new Date(2020, 2, 5)
+    },
+    {
+        id: 'e13',
+        title: 'Trash Can',
+        amount: 30.00,
+        date: new Date(2021, 10, 14)
+    },
+    {
+        id: 'e14', title: 'Headphones',
+        amount: 40.95,
+        date: new Date(2021, 6, 27)
+    },
+    {
+        id: 'e15',
+        title: 'Wetsuit',
+        amount: 37.18,
+        date: new Date(2019, 10, 14)
+    },
+    {
+        id: 'e16',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2020, 5, 12)
+    },
+    {
+        id: 'e17',
+        title: 'Sunscreen',
+        amount: 15.12,
+        date: new Date(2022, 6, 28)
+    },
+    {
+        id: 'e18', title: 'Water Filter',
+        amount: 53.67,
+        date: new Date(2023, 3, 10)
+    },
+    {
+        id: 'e19',
+        title: 'Jump Rope',
+        amount: 6.50,
+        date: new Date(2023, 8, 19)
+    },
+    {
+        id: 'e20',
+        title: 'Jogging Backpack)',
+        amount: 29.99,
+        date: new Date(2021, 5, 17)
+    },
+    {
+        id: 'e21',
+        title: 'Body Wash',
+        amount: 14.19,
+        date: new Date(2019, 1, 14)
+    },
+    {
+        id: 'e22', title: 'Pomade',
+        amount: 22.22,
+        date: new Date(2019, 9, 9)
+    },
+    {
+        id: 'e23',
+        title: 'Air pump',
+        amount: 13.99,
+        date: new Date(2021, 12, 1)
+    },
+    {
+        id: 'e24',
+        title: 'Heavy Bag',
+        amount: 374.55,
+        date: new Date(2023, 7, 5)
+    },
 ];
 
 export default expenses;
