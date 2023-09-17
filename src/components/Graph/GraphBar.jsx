90fcdccffb6d06f68e14922c6b321074dc923895
Ryan Valera <valeraryan@gmail.com>
2023-09-16 18:41:56 -0700

    Created the fillGraphBar variable which will be passed in the height percentage passed into the 'graph-bar__fill' div container. This will be modified each time the GraphBar component is called on loop and sets it's value with the expression: 'props.value / props.maxValue) * 100'.

--- src/components/Graph/GraphBar.jsx
+++ src/components/Graph/GraphBar.jsx
@@ -1,6 +1,14 @@
 import './GraphBar.css'
 
 function GraphBar(props) {
+    // The fillGraphBar variable will be passed as a (style) 'height: percentage' filling a div container for each month
+    let fillGraphBar = '0%';
+
+    // If props.value and props.maxValue are equal then 100% of the bar is filled
+    // If props.value is 25 and props.maxValue is 100 then 25% of the bar is filled
+    if (props.maxValue > 0) {
+        fillGraphBar = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
 
     return (
         <div className='graph-bar'>
