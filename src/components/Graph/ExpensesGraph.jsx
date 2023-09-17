831305e17f51269e12781fa4128904ec03f57c27
Ryan Valera <valeraryan@gmail.com>
2023-09-16 19:13:35 -0700

    Added a loop block to the ExpensesGraph.jsx component/function that will loop through the filtered array 'expenseData' passed from Expenses.jsx. This extracts and sums up all values for each month exclusively and takes that number-value, assigning it to the value attribute for each object's month inside of 'graphData'. The newly populated graphData array is then passed into the Graph component as a prop.

--- src/components/Graph/ExpensesGraph.jsx
+++ src/components/Graph/ExpensesGraph.jsx
@@ -1,6 +1,7 @@
 import Graph from './Graph';
 
 function ExpensesGraph(props) {
+    // create an array of labels/values which will be poplulated by iterating props.expenseData in the for loop below
     const graphData = [
         { label: 'Jan', value: 0 },
         { label: 'Feb', value: 0 },
@@ -16,7 +17,19 @@ function ExpensesGraph(props) {
         { label: 'Dec', value: 0 },
     ];
 
-    return;
+    props.expenseData.forEach(expense => {
+
+        // Loop through the filtered array (by year) passed down by ExpensesGraph
+        // Abstract ANY expense by month using getMonth() returning a numerical representation starting at 0 (January)
+        const month = expense.date.getMonth(); // starting at 0 => January => 0
+
+        // Use the number abstracted by getMonth to index graphData object and set the value, incrementing a total
+        // This total represents the sum of all amounts and expenses for that given month
+        graphData[month].value += expense.amount;
+    })
+
+    // Once data is populated into graphData, pass it as a prop value through the Graph component
+    return <Graph graphData={graphData} />;
 }
 
 export default ExpensesGraph;
