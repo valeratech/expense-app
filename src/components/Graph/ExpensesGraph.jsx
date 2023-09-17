921f72213ec6f3b5d4c1f6ebf9283d2fa73b7ac6
Ryan Valera <valeraryan@gmail.com>
2023-09-16 19:00:38 -0700

    Created and added a new array of objects inside of ExpensesGraph.jsx component which . The objects will be populated with total amount/value for each month that was filtered from the Expenses.jsx props component 'expenseData' as well as a label that abbreviates each month to be used as a caption in the bar-graph.

--- src/components/Graph/ExpensesGraph.jsx
+++ src/components/Graph/ExpensesGraph.jsx
@@ -1,7 +1,22 @@
 import Graph from './Graph';
 
-function ExpensesGraph() {
+function ExpensesGraph(props) {
+    const graphData = [
+        { label: 'Jan', value: 0 },
+        { label: 'Feb', value: 0 },
+        { label: 'Mar', value: 0 },
+        { label: 'Apr', value: 0 },
+        { label: 'May', value: 0 },
+        { label: 'Jun', value: 0 },
+        { label: 'Jul', value: 0 },
+        { label: 'Aug', value: 0 },
+        { label: 'Sep', value: 0 },
+        { label: 'Oct', value: 0 },
+        { label: 'Nov', value: 0 },
+        { label: 'Dec', value: 0 },
+    ];
 
+    return;
 }
 
 export default ExpensesGraph;
