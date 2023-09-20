4bd2a3278124a9c9ba66ed4f90ff7552dadb2441
Ryan Valera <valeraryan@gmail.com>
2023-09-20 16:13:22 -0700

    Removed parseInt from the prop: <div> 'expense-item__price' in ExpenseItem.jsx causing the amount/value to display in whole-numbers.

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,13 +4,12 @@ import Card from "../shared/Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    console.log(typeof props.item.amount);
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.item.date} />
             <div className="expense-item__description">
                 <h2>{props.item.title}</h2>
-                <div className="expense-item__price">${parseInt(props.item.amount.toFixed(2))}</div>
+                <div className="expense-item__price">${props.item.amount.toFixed(2)}</div>
             </div>
         </Card>
     )
